dba94690f126cdf3687b168f98a2b96f03b87ca2
Andrew Wright <anwright@davidson.edu>
2018-08-01 10:50:06 -0400

    presentation ready

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,6 +1,6 @@
 class Painting < ApplicationRecord
 
-	belongs_to :painting
+	# belongs_to :painting
 	mount_uploader :image, ImageUploader
 
 	# has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
